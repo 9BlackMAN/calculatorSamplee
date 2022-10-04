8212bfba3a659ff1d228b04f3701457e5d9aa6c5
ckc <can.cakici.91@gmail.com>
2022-10-04 17:20:47 +0300

    second commit

--- app/src/main/java/com/ckc/calculatorsample/MainActivity.kt
+++ app/src/main/java/com/ckc/calculatorsample/MainActivity.kt
@@ -2,10 +2,77 @@ package com.ckc.calculatorsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.ckc.calculatorsample.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    var numberFirst :Float? = null
+    var numberSecond :Float? = null
+    var result : Float? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+
+
+
+
     }
+    fun sum(view:View){
+
+        numberFirst = binding.number1.text.toString().toFloatOrNull()
+        numberSecond = binding.number2.text.toString().toFloatOrNull()
+        if (numberFirst==null || numberSecond==null){
+            Toast.makeText(this,"Error !!!",Toast.LENGTH_LONG).show()
+        }else{
+            result = numberFirst!! + numberSecond!!
+            binding.result.text = "Result : ${result}"
+        }
+
+
+    }
+    fun sub(view:View){
+        numberFirst = binding.number1.text.toString().toFloatOrNull()
+        numberSecond = binding.number2.text.toString().toFloatOrNull()
+        if (numberFirst==null || numberSecond==null){
+            Toast.makeText(this,"Error !!!",Toast.LENGTH_LONG).show()
+        }else{
+            result = numberFirst!! - numberSecond!!
+            binding.result.text = "Result : ${result}"
+        }
+
+    }
+    fun multi(view:View){
+        numberFirst = binding.number1.text.toString().toFloatOrNull()
+        numberSecond = binding.number2.text.toString().toFloatOrNull()
+        if (numberFirst==null || numberSecond==null){
+            Toast.makeText(this,"Error !!!",Toast.LENGTH_LONG).show()
+        }else{
+            result = numberFirst!! * numberSecond!!
+            binding.result.text = "Result : ${result}"
+        }
+
+    }
+    fun div(view:View){
+        numberFirst = binding.number1.text.toString().toFloatOrNull()
+        numberSecond = binding.number2.text.toString().toFloatOrNull()
+        if (numberFirst==null || numberSecond==null){
+            Toast.makeText(this,"Error !!!",Toast.LENGTH_LONG).show()
+        }else{
+            result = numberFirst!! / numberSecond!!
+            binding.result.text = "Result : ${result}"
+        }
+
+    }
+
+
 }
